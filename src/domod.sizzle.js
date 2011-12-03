fa9f79c1b1933393e5e9bcfcf79165ccbe5543f1
Cahnory <cahnory@gmail.com>
2011-12-03 18:31:25 +0100

    Targeter filters doublons and new method
    Targeter filters doublons and a "closest" method has been added

--- src/domod.sizzle.js
+++ src/domod.sizzle.js
@@ -7,6 +7,11 @@
 			for(var i = 0; i < arguments.length; i++) {
 				// Keep element and document nodes (1 & 9)
 				if(arguments[i] !== null && arguments[i] !== undefined && arguments[i].nodeType%8 === 1) {
+					for(var j = i+1; j < arguments.length; j++) {
+						if (arguments[i] === arguments[j]) {
+							j	= ++i;
+						}
+					}
 					items.push(arguments[i]);
 				}
 			}
@@ -38,6 +43,20 @@
 			});
 		}
 		return	$.apply(this, items);
+	};
+	
+	$.fn.closest = function(selector) {
+		var nodes	= [];
+		this.each(function() {
+			var parent	= this;
+			while(parent = parent.parentNode) {
+				if(Sizzle.filter(selector, [parent]).length) {
+					nodes.push(parent);
+					break;
+				}
+			}
+		});
+		return	$.apply(this, nodes);
 	};
 	
 	// Remove elements that match the selector
